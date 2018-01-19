e64233017e8bcaf46aab457e389a2bb93623f9de
ChrisJeamme <christopher@jeamme.fr>
2018-01-19 17:48:27 +0100

    add Program class with the base program

---
+++ src/main/java/fr/ptdq/interoperability/Program.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ptdq.interoperability;
+
+/**
+ *
+ * @author chris
+ */
+public class Program
+{
+
+    public static String question(String question)
+    {
+        System.out.println("Question = " + question);
+        return "Réponse pas encore implémenté.";
+    }
+}
