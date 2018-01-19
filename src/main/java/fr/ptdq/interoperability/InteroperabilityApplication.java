c4d77483e9377a2c53f628842ce2ec0dc76655e9
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-01-19 09:54:52 +0100

    initializtion of the project

---
+++ src/main/java/fr/ptdq/interoperability/InteroperabilityApplication.java
@@ -0,0 +1,12 @@
+package fr.ptdq.interoperability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteroperabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteroperabilityApplication.class, args);
+	}
+}
