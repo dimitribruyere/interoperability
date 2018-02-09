159ada748d6a307500b0056b7f664975b1fd429c
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-02-09 12:07:18 +0100

    feat: begining of the method to get memebers

--- src/main/java/fr/ptdq/interoperability/HTMLparser/HTMLparser.java
+++ src/main/java/fr/ptdq/interoperability/HTMLparser/HTMLparser.java
@@ -19,21 +19,12 @@ public class HTMLparser
 {
 
     /**
-     * @return List<String>
-     */
-    public static List<String> getTeams()
-    {
-        String url = "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams.html";
-        return getTeamsWithUrl(url);
-    }
-    
-
-    /**
      * @param url String
      * @return List<String>
      */
-    private static List<String> getTeamsWithUrl(String url)
+    static List<String> getTeams()
     {
+        String url = "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams.html";
         List<String> teamList = new ArrayList<>();
         try
         {
@@ -43,9 +34,9 @@ public class HTMLparser
             for (int i = 0; i < teamSet.size(); i++)
             {
                 String team = teamSet.get(i).select("strong").text();
-                
+
                 team = team.split("[(]")[0];
-                
+
                 teamList.add(team);
             }
             return teamList;
@@ -55,4 +46,51 @@ public class HTMLparser
         }
         return null;
     }
+
+    /**
+     * @param url String
+     *
+     * @return List<String>
+     */
+    private static List<String> getMember()
+    {
+
+        String listUrl[] =
+        {
+            "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams/micro-nano-structuring/staff.html",
+            "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams/radiation-matter-interaction/staff.html",
+            "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams/image-science-computer-vision/staff.html",
+            "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams/data-intelligence/staff.html",
+            "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams/connected-intelligence/staff.html",
+            "https://laboratoirehubertcurien.univ-st-etienne.fr/en/teams/secure-embedded-systems-hardware-architectures/staff.html"
+        };
+
+        List<String> teamList = new ArrayList<>();
+
+        for (int j = 0; j <listUrl.length; j++)
+        {
+            String url = listUrl[j];
+            try
+            {
+                Document doc = Jsoup.connect(url).get();
+
+//                Elements members = doc.select(".simple");
+//                for (int i = 0; i < members.size(); i++)
+//                {
+//                    String team = members.get(i).select("strong").text();
+//
+//                    team = team.split("[(]")[0];
+//
+//                    teamList.add(team);
+//                }
+                return teamList;
+            } catch (IOException e)
+            {
+                System.err.println("La recherche avec cet URL n'a rien trouvé");
+            }
+            return null;
+        }
+        return null;
+    }
+
 }
