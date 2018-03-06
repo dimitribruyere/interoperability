2985ed2bbd17ee9d3ef771dfaf1889cedb264518
Chris <christopher@jeamme.fr>
2018-03-06 02:49:42 +0100

    feat: JWBF lib works with a test

--- src/main/java/fr/ptdq/interoperabilty/wikidata/TestJWBF.java
+++ src/main/java/fr/ptdq/interoperabilty/wikidata/TestJWBF.java
@@ -8,8 +8,9 @@ package fr.ptdq.interoperabilty.wikidata;
 import java.util.concurrent.TimeUnit;
 import net.sourceforge.jwbf.core.actions.HttpActionClient;
 import net.sourceforge.jwbf.core.contentRep.Article;
+import net.sourceforge.jwbf.core.contentRep.SimpleArticle;
+import net.sourceforge.jwbf.mediawiki.actions.queries.BaseQuery;
 import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
-import org.apache.http.impl.client.HttpClientVersion;
 
 /**
  *
@@ -23,16 +24,22 @@ public class TestJWBF
      */
     public static void main(String[] args)
     {
-        org.apache.http.impl.client.HttpClientBuilder.create();
+        //org.apache.http.impl.client.HttpClientBuilder.create();
 
         HttpActionClient client = HttpActionClient.builder() //
                 .withUrl("https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/") //
                 .withUserAgent("PTDQ_Bot", "1.0", "PTDQ@ptdq.com") //
                 .withRequestsPerUnit(10, TimeUnit.MINUTES) //
                 .build();
-        // HITCHHIKER'S GUIDE TO THE GALAXY FANS
+        
+        MediaWikiBot wikiBot = new MediaWikiBot("https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/");
+        wikiBot.login("ChristopherJEAMME", "D2rZ9Fk1Sg3K");
+        //SimpleArticle article = wikiBot.readData("Main_Page");
+        //System.out.println(wikiBot.getWikiType());
+
+        System.out.println(wikiBot.readDataOpt("Item:Q904")); //Pierre Maret
+        
         //applyChangesTo(article);
-        //wikiBot.login("ChristopherJEAMME", "D2rZ9Fk1Sg3K");
         //article.save();
     }
 
