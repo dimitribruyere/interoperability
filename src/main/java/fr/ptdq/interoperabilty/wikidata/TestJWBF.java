d02112c80b58e198ae7c87a62e21dbf02896aac6
Chris <christopher@jeamme.fr>
2018-03-09 01:24:57 +0100

    feat: add some demo in TestJWBF

--- src/main/java/fr/ptdq/interoperabilty/wikidata/TestJWBF.java
+++ src/main/java/fr/ptdq/interoperabilty/wikidata/TestJWBF.java
@@ -5,12 +5,21 @@
  */
 package fr.ptdq.interoperabilty.wikidata;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import net.sourceforge.jwbf.core.actions.HttpActionClient;
+import net.sourceforge.jwbf.core.bots.WikiBot;
 import net.sourceforge.jwbf.core.contentRep.Article;
 import net.sourceforge.jwbf.core.contentRep.SimpleArticle;
 import net.sourceforge.jwbf.mediawiki.actions.queries.BaseQuery;
+import net.sourceforge.jwbf.mediawiki.actions.queries.Search;
 import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot;
+import org.json.JSONObject;
 
 /**
  *
@@ -22,29 +31,80 @@ public class TestJWBF
     /**
      * Sample bot that retrieves and edits an article.
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        //org.apache.http.impl.client.HttpClientBuilder.create();
-
-        HttpActionClient client = HttpActionClient.builder() //
-                .withUrl("https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/") //
-                .withUserAgent("PTDQ_Bot", "1.0", "PTDQ@ptdq.com") //
-                .withRequestsPerUnit(10, TimeUnit.MINUTES) //
-                .build();
-        
+        //Connexion
         MediaWikiBot wikiBot = new MediaWikiBot("https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/");
-        wikiBot.login("user", "pass");
-        //SimpleArticle article = wikiBot.readData("Main_Page");
-        //System.out.println(wikiBot.getWikiType());
-
-        System.out.println(wikiBot.readDataOpt("Item:Q904")); //Pierre Maret
+        wikiBot.login("ChristopherJEAMME", "");
         
-        //applyChangesTo(article);
-        //article.save();
-    }
+        //Récupération d'un object
+        Article article= wikiBot.getArticle("Item:Q904");
+        
+        //Transformation du JSON en objet
+        JSONObject obj = new JSONObject(article.getText());
+        
+        //Récupération des déclarations
+        JSONObject declarations = obj.getJSONObject("claims");
+        
+        //Lecture des déclarations
+        System.out.println(declarations.toString(2));
+        
+        //Suppressions des 3 déclarations
+        obj.getJSONObject("claims").remove("P252");
+        obj.getJSONObject("claims").remove("P186");
+        obj.getJSONObject("claims").remove("P243");
 
-    static void applyChangesTo(Article article)
-    {
-        // edits the article...
+        //Affichage de l'objet après suppressions
+        System.out.println(obj.toString(2));
+        
+        //Appliquation du JSON à l'objet Article
+        article.setText(obj.toString());
+
+        //Envoi des modifications au wiki
+        //article.save();
+        
+        //Si il s'agit d'un nouvel article
+        SimpleArticle simpleArticle = new SimpleArticle(obj.toString(), "Item:Q999");
+        
+        //Envoi du nouvel article au wiki
+        //wikiBot.writeContent(simpleArticle);
+        
+        //Lancement d'une requete        
+        HttpURLConnection httpcon = (HttpURLConnection) ((new URL("https://wdaqua-qanary.univ-st-etienne.fr/gerbil-execute/wdaqua-core1, QueryExecuter/").openConnection()));
+        httpcon.setDoOutput(true);
+        httpcon.setRequestProperty("Content-Type", "application/json");
+        httpcon.setRequestProperty("Accept", "application/json");
+        httpcon.setRequestMethod("POST");
+        httpcon.connect();
+
+        byte[] outputBytes = "query=wife of Barack Obama&lang=en&kb=dbpedia".getBytes("UTF-8");
+        OutputStream os = httpcon.getOutputStream();
+        os.write(outputBytes);
+        os.close();
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null)
+        {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.println("REPONSE REQUETE="+response.toString());
+
+
+        
+        
+        
+        //TESTS
+        
+//        System.out.println("SUMMARY="+article.getEditSummary());
+//        System.out.println("TEXT="+article.getText());
+//        System.out.println("EDITOR="+article.getEditor());
+//        System.out.println("TITLE="+article.getTitle());
+//        System.out.println(wikiBot.getWikiType());
+//        System.out.println(wikiBot.readDataOpt("Item:Q904"));
+      
+        
     }
 }
