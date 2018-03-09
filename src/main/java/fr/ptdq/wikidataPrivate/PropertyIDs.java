231192a35f63290a007b42e74a34e35dbf541a85
KHAMED Smaïl <khameds@gmail.com>
2018-03-09 10:09:44 +0100

    feat: define static attributs to access properties IS easily.

---
+++ src/main/java/fr/ptdq/wikidataPrivate/PropertyIDs.java
@@ -0,0 +1,11 @@
+package fr.ptdq.wikidataPrivate;
+
+public class PropertyIDs
+{
+    public static String Personne  = "P";
+    public static String Prenom  = "P";
+    public static String Adresse  = "P257";
+    public static String EquipeDeRecherche  = "P289";
+    public static String Fonction  =   "P258";
+    public static String Membre  =   "P292";
+}
