87439e2c752ddab24f8688dcc16d398abd6993c3
KHAMED Smaïl <khameds@gmail.com>
2018-03-09 10:34:32 +0100

    feat: Properties ID.

--- src/main/java/fr/ptdq/wikidataPrivate/PropertyIDs.java
+++ src/main/java/fr/ptdq/wikidataPrivate/PropertyIDs.java
@@ -2,8 +2,9 @@ package fr.ptdq.wikidataPrivate;
 
 public class PropertyIDs
 {
-    public static String Personne  = "P";
-    public static String Prenom  = "P";
+    public static String Personne  = "Q886";
+    public static String Prenom  = "P252";
+    public static String Nom  = "P243";
     public static String Adresse  = "P257";
     public static String EquipeDeRecherche  = "P289";
     public static String Fonction  =   "P258";
