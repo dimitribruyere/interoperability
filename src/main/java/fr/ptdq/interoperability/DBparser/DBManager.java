1feeb9a19b31853c98fbafc3bdd6235c9666baf3
KHAMED Smaïl <khameds@gmail.com>
2018-03-11 10:02:11 +0100

    feat : Adding LHC's staff. (ID exceptions)

--- src/main/java/fr/ptdq/interoperability/DBparser/DBManager.java
+++ src/main/java/fr/ptdq/interoperability/DBparser/DBManager.java
@@ -1,6 +1,22 @@
 package fr.ptdq.interoperability.DBparser;
 
+import fr.ptdq.wikidataPrivate.PropertyIDs;
+import org.wikidata.wdtk.datamodel.helpers.Datamodel;
+import org.wikidata.wdtk.datamodel.helpers.ItemDocumentBuilder;
+import org.wikidata.wdtk.datamodel.helpers.StatementBuilder;
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
+import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.PropertyDocument;
+import org.wikidata.wdtk.util.WebResourceFetcherImpl;
+import org.wikidata.wdtk.wikibaseapi.ApiConnection;
+import org.wikidata.wdtk.wikibaseapi.LoginFailedException;
+import org.wikidata.wdtk.wikibaseapi.WikibaseDataEditor;
+import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DBManager
 {
@@ -51,7 +67,6 @@ public class DBManager
         try
         {
             ResultSetMetaData rsmd = rs.getMetaData();
-            System.out.println("querying SELECT * FROM XXX");
             int columnsNumber = rsmd.getColumnCount();
             while (rs.next())
             {
@@ -71,15 +86,113 @@ public class DBManager
 
     public static void main(String[] args) throws Exception
     {
+
+
+        /**
+         * Personne     P
+         * Nom          P
+         * Prénom       P
+         * Fonction     P258
+         * Adresse      P257
+         * Equipe de recherche  P289
+         * Membre de    P292
+         *
+         */
         Connection conn = connect();
 
         if (conn != null)
             System.out.println("Connection to DB : Success.");
 
-        ResultSet rs = executeQuery(conn, "SELECT * FROM Personne");
+        registerPersons(conn);
 
-        displayResultSet(rs);
+
     }
 
+    public static void registerPersons(Connection conn) throws Exception
+    {
+        String siteIri = "https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/index.php/";
+        ArrayList<String> listPers = new ArrayList<>();
+        ResultSet rs = executeQuery(conn,"SELECT nom, prenom FROM Personne");
+
+        try
+        {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+
+            while (rs.next())
+            {
+                String pers = new String("");
+                for (int i = 1; i <= columnsNumber; i++)
+                {
+                    if (i > 1) pers += "/";
+                    pers += rs.getString(i);
+                }
+                listPers.add(pers);
+            }
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+        for(String s : listPers)
+        {
+            System.out.println(s);
+        }
+
+        /*** WIKI DATA ***/
+
+        WebResourceFetcherImpl.setUserAgent("Wikidata Toolkit EditOnlineDataExample");
+
+        ApiConnection con = new ApiConnection("https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/api.php");
+
+        try {
+            //Put in the first place the user with which you created the bot account
+            //Put as password what you get when you create the bot account
+            con.login("Root@SamBot", "tcr0kgob5hgjejp2rrga8kocjq3jfc0l");
+        } catch (LoginFailedException e) {
+            e.printStackTrace();
+        }
+
+        //Example for getting information about an entity, here the example of The Laboratoire Huber Curien, Q900
+        //For more examples give a look at: https://github.com/Wikidata/Wikidata-Toolkit-Examples/blob/master/src/examples/FetchOnlineDataExample.java
+        WikibaseDataFetcher wbdf = new WikibaseDataFetcher(con, siteIri);
+        ItemDocument personnelHC = (ItemDocument) wbdf.getEntityDocument(PropertyIDs.Personne);
+
+        WikibaseDataEditor wbde = new WikibaseDataEditor(con, siteIri);
+        PropertyDocument propertyNom = (PropertyDocument) wbdf.getEntityDocument(PropertyIDs.Nom);
+        PropertyDocument propertyPrenom = (PropertyDocument) wbdf.getEntityDocument(PropertyIDs.Prenom);
+
+        ItemIdValue itemPersonnelID = personnelHC.getItemId();
+        System.out.println("ItemIdValue = " + itemPersonnelID);
+        String[] sub;
+
+        for(int i = 0 ; i < listPers.size() ; i++)
+        {
+            sub = listPers.get(i).split("/"); // 0 = nom, 1 = prenom
+            System.out.println("ADDING [nom = " + sub[0] + " prenom = " + sub[1] + "]");
+            org.wikidata.wdtk.datamodel.interfaces.Statement statement1 = StatementBuilder
+                    .forSubjectAndProperty(itemPersonnelID, propertyNom.getPropertyId())
+                    .withValue(Datamodel.makeStringValue(sub[0])).build();
+            org.wikidata.wdtk.datamodel.interfaces.Statement statement2 = StatementBuilder
+                    .forSubjectAndProperty(itemPersonnelID, propertyPrenom.getPropertyId())
+                    .withValue(Datamodel.makeStringValue(sub[1])).build();
+
+            ItemDocument itemDocument = ItemDocumentBuilder.forItemId(itemPersonnelID)
+                    .withLabel(sub[1] + " " + sub[0], "en")
+                    .withLabel(sub[1] + " " + sub[0], "fr")
+                    .withStatement(statement1)
+                    .withStatement(statement2).build();
+
+            try
+            {
+                ItemDocument newItemDocument = wbde.createItemDocument(itemDocument,
+                                                                       "Personnel du Laboratoire Hubert Curien.");
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 }
