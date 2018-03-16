5ead32c7913c5c39ab5fdcb21d506f397e90a24a
KHAMED Smaïl <khameds@gmail.com>
2018-03-16 08:28:17 +0100

    feat: Add LaboHC prop. ID

--- src/main/java/fr/ptdq/wikidataPrivate/PropertyIDs.java
+++ src/main/java/fr/ptdq/wikidataPrivate/PropertyIDs.java
@@ -5,6 +5,7 @@ public class PropertyIDs
     public static String Personne  = "Q886";
     public static String Prenom  = "P252";
     public static String Nom  = "P243";
+    public static String LaboHC  = "Q900";
     public static String Adresse  = "P257";
     public static String EquipeDeRecherche  = "P289";
     public static String Fonction  =   "P258";
