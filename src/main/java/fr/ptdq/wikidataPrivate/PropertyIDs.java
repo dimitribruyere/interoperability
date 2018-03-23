cf9e15370d49270d1c92dce5ba21c800e08168f9
KHAMED Smaïl <khameds@gmail.com>
2018-03-23 12:02:15 +0100

    feat : Add teams' ID.

--- src/main/java/fr/ptdq/wikidataPrivate/PropertyIDs.java
+++ src/main/java/fr/ptdq/wikidataPrivate/PropertyIDs.java
@@ -8,12 +8,21 @@ public class PropertyIDs
     public static String InstanceDe = "P186";
     public static String LaboHC  = "Q900";
     public static String Adresse  = "P257";
-    public static String EquipeDeRecherche  = "P289";
+    public static String EquipeDeRecherche  = "Q910";
     public static String Fonction  =   "P258";
     public static String Membre  =   "P292";
     public static String Publication = "Q941";    
     public static String Auteur = "P298";
     public static String année = "P167";
+
+    /*** Equipes de recherche ***/
+    public static String EquipeMicro  = "Q1227";
+    public static String EquipeLaser  = "Q1228";
+    public static String EquipeImage  = "Q1229";
+    public static String EquipeData  = "Q1230";
+    public static String EquipeSecure  = "Q1232";
+    public static String EquipeConnected  = "Q1231";
+
 
 
 }
