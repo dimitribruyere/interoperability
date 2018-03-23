0772b7ae5d7df729dd54014683b3ec6d9f22de40
KHAMED Smaïl <khameds@gmail.com>
2018-03-23 12:03:52 +0100

    feat : Add persons and related research team.

--- src/main/java/fr/ptdq/interoperability/DBparser/DBManager.java
+++ src/main/java/fr/ptdq/interoperability/DBparser/DBManager.java
@@ -95,7 +95,7 @@ public class DBManager
         if (conn != null)
             System.out.println("Connection to DB : Success.");
 
-
+        //getEquipeRecherche(conn, "Muhlenbach","Fabrice");
         //getPersonne(conn, "Alata", "O");
         //registerTeams(conn);
         registerPersons(conn);
@@ -157,10 +157,17 @@ public class DBManager
         //Example for getting information about an entity, here the example of The Laboratoire Huber Curien, Q900
         //For more examples give a look at: https://github.com/Wikidata/Wikidata-Toolkit-Examples/blob/master/src/examples/FetchOnlineDataExample.java
         WikibaseDataFetcher wbdf = new WikibaseDataFetcher(con, siteIri);
+
+
+        /*** Item documents ***/
         ItemDocument personnelHC = (ItemDocument) wbdf.getEntityDocument(PropertyIDs.Personne);
+
 
         WikibaseDataEditor wbde = new WikibaseDataEditor(con, siteIri);
 
+
+
+        /*** Properties ***/
         PropertyDocument propertyInstanceDe = (PropertyDocument) wbdf.getEntityDocument(PropertyIDs.InstanceDe);
         PropertyDocument propertyMembre = (PropertyDocument) wbdf.getEntityDocument(PropertyIDs.Membre);
 
@@ -173,7 +180,7 @@ public class DBManager
 
 
 
-        for(int i = 1 ; i < listPers.size() ; i++)
+        for(int i = 0 ; i < listPers.size() ; i++)
         {
             sub = listPers.get(i).split("/"); // 0 = nom, 1 = prenom
             System.out.println("ADDING [nom = " + sub[0] + " prenom = " + sub[1] + "]");
@@ -181,15 +188,30 @@ public class DBManager
                     .forSubjectAndProperty(itemPersonnelID, propertyInstanceDe.getPropertyId())
                     .withValue(personnelHC.getItemId()).build();
 
-            org.wikidata.wdtk.datamodel.interfaces.Statement statement2 = StatementBuilder
-                    .forSubjectAndProperty(itemPersonnelID, propertyInstanceDe.getPropertyId())
-                    .withValue(personnelHC.getItemId()).build();
+            String id = getEquipeID(getEquipeRecherche(conn,sub[0],sub[1]));
+            ItemDocument itemDocument;
+            if(id .equals(""))
+            {
+                itemDocument = ItemDocumentBuilder.forItemId(itemPersonnelID)
+                        // .withLabel(sub[1] + " " + sub[0], "en")
+                        .withLabel(sub[1] + " " + sub[0], "fr")
+                        .withStatement(statement1)
+                        .build();
+            }
+            else
+            {
+                ItemDocument equipeRecherche = (ItemDocument) wbdf.getEntityDocument(id);
+                org.wikidata.wdtk.datamodel.interfaces.Statement statement2 = StatementBuilder
+                        .forSubjectAndProperty(itemPersonnelID, propertyMembre.getPropertyId())
+                        .withValue(equipeRecherche.getItemId()).build();
 
 
-            ItemDocument itemDocument = ItemDocumentBuilder.forItemId(itemPersonnelID)
-                   // .withLabel(sub[1] + " " + sub[0], "en")
-                    .withLabel(sub[1] + " " + sub[0], "fr")
-                    .withStatement(statement1).build();
+                itemDocument = ItemDocumentBuilder.forItemId(itemPersonnelID)
+                        // .withLabel(sub[1] + " " + sub[0], "en")
+                        .withLabel(sub[1] + " " + sub[0], "fr")
+                        .withStatement(statement1)
+                        .withStatement(statement2).build();
+            }
 
             try
             {
@@ -228,6 +250,66 @@ public class DBManager
         }
 
         /*** WIKI DATA ***/
+
+        WebResourceFetcherImpl.setUserAgent("Wikidata Toolkit EditOnlineDataExample");
+
+        ApiConnection con = new ApiConnection("https://wdaqua-biennale-design.univ-st-etienne.fr/wikibase/api.php");
+
+        try {
+            //Put in the first place the user with which you created the bot account
+            //Put as password what you get when you create the bot account
+            con.login("Root@SamBot", "tcr0kgob5hgjejp2rrga8kocjq3jfc0l");
+        } catch (LoginFailedException e) {
+            e.printStackTrace();
+        }
+
+        //Example for getting information about an entity, here the example of The Laboratoire Huber Curien, Q900
+        //For more examples give a look at: https://github.com/Wikidata/Wikidata-Toolkit-Examples/blob/master/src/examples/FetchOnlineDataExample.java
+        WikibaseDataFetcher wbdf = new WikibaseDataFetcher(con, siteIri);
+
+
+        /*** Item documents ***/
+        ItemDocument equipeHc = (ItemDocument) wbdf.getEntityDocument(PropertyIDs.EquipeDeRecherche);
+
+
+        WikibaseDataEditor wbde = new WikibaseDataEditor(con, siteIri);
+
+
+
+        /*** Properties ***/
+        PropertyDocument propertyInstanceDe = (PropertyDocument) wbdf.getEntityDocument(PropertyIDs.InstanceDe);
+        ItemIdValue itemEquipeID = ItemIdValue.NULL;
+
+        // System.out.println("ItemIdValue = " + itemPersonnelID);
+        String[] sub;
+
+
+
+        for(int i = 0 ; i < listTeam.size() ; i++)
+        {
+
+            org.wikidata.wdtk.datamodel.interfaces.Statement statement1 = StatementBuilder
+                    .forSubjectAndProperty(itemEquipeID, propertyInstanceDe.getPropertyId())
+                    .withValue(equipeHc.getItemId()).build();
+
+
+
+
+            ItemDocument itemDocument = ItemDocumentBuilder.forItemId(itemEquipeID)
+                    // .withLabel(sub[1] + " " + sub[0], "en")
+                    .withLabel(listTeam.get(i), "fr")
+                    .withStatement(statement1).build();
+
+            try
+            {
+                ItemDocument newItemDocument = wbde.createItemDocument(itemDocument,
+                                                                       "Equipe de recherche du Laboratoire Hubert Curien.");
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
 
         // TODO : add to wikidata when ID pb will be adressed
     }
@@ -297,6 +379,62 @@ public class DBManager
         }
 
         return result;
+    }
+
+    public static ArrayList<String> getEquipeRecherche(Connection conn, String nom, String prenom)
+    {
+        ArrayList<String> result = new ArrayList<>();
+
+        ResultSet rs = executeQuery(conn,"SELECT Equipe_Recherche.nom FROM Equipe_Recherche, Personne, Membre_Equipe_Recherche WHERE Personne.nom LIKE '" + nom + "' AND Personne.prenom LIKE '" + prenom
+                +"' AND Equipe_Recherche.id = Membre_Equipe_Recherche.id_equipe_recherche AND Personne.id = Membre_Equipe_Recherche.id_personne" );
+
+        try
+        {
+
+            while (rs.next())
+            {
+                String team = rs.getString("nom");
+
+                result.add(team);
+            }
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+        for(String s : result)
+        {
+            System.out.println(s);
+        }
+
+        return result;
+    }
+
+    public static String getEquipeID(ArrayList<String> listeEquipe)
+    {
+            if(listeEquipe == null )
+            {
+                System.out.println("[Error] team list null.");
+                return "";
+            }
+
+            if(listeEquipe.isEmpty())
+                return "";
+
+            if(listeEquipe.get(0).equals("Micro & nano structuring team"))
+                return PropertyIDs.EquipeMicro;
+        if(listeEquipe.get(0).equals("Laser-matter interaction team"))
+            return PropertyIDs.EquipeLaser;
+        if(listeEquipe.get(0).equals("Image science team"))
+            return PropertyIDs.EquipeImage;
+        if(listeEquipe.get(0).equals("Data Intelligence team"))
+            return PropertyIDs.EquipeData;
+        if(listeEquipe.get(0).equals("Connected Intelligence team"))
+            return PropertyIDs.EquipeConnected;
+        if(listeEquipe.get(0).equals("Secure Embedded Systems & Hardware Architectures team"))
+            return PropertyIDs.EquipeSecure;
+
+        return ""; // default
     }
 }
 
